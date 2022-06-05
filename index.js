324654f52ff8e0c395d1b9e10e5fc7e48f1e6fcf
Lendy10 <lendy.pradhana.hartono-2016@fst.unair.ac.id>
2022-06-05 08:00:59 +0700

    finalisasi challange 5

--- index.js
+++ index.js
@@ -6,15 +6,27 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, callback) => {
+  const dataSorting = callback(data)
+  const containerSorter = []
+  for (counter = 0; counter < data.length; counter++) {
+    containerSorter.push(`${counter+1}. ${dataSorting[counter]}`)
+  }
+
+  return containerSorter
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  return data.sort()
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  return data.sort().reverse()
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
